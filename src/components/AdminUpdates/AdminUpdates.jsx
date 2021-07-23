9c389f46923ba7ae7592a281d91d37fc7a9f354b
Alex Kim <alexkim251@yahoo.com>
2021-07-23 16:00:34 -0500

    added handle page select logic for conidtional render true false value selection

--- src/components/AdminUpdates/AdminUpdates.jsx
+++ src/components/AdminUpdates/AdminUpdates.jsx
@@ -5,13 +5,49 @@ import Select from '@material-ui/core'
 export default function AdminUpdates() {
 
   //holds value of selection to for page render conditionals
-  let [selectedPage,setSelectedPage] = useState("")
+  let [selectedPage,setSelectedPage] = useState("");
+
+  let [updateLicenseesRender,setUpdateLicenseesRender] = useState(false);
+  let [addFloorAndPlacementTypesRender,setAddFloorAndPlacementTypesRender] = useState(false);
+  let [updateShippingCostsRender,setUpdateShippingCostsRender] = useState(false);
+  let [updateMaterialCostsAndInventoryRender,setUpdateMaterialCostsAndInventoryRender] = useState(false);
+  let [systemAdminRender,setSystemAdminRender] = useState(false);
 
   //handles values of selected page change on event change
   const handleSelectPage = (event) => {
-    setSelectedPage(event.target.value)
-
-
+    setSelectedPage(event.target.value);
+    // logic for activating render based on selected value
+    if (selectedPage == 1) {
+      setUpdateLicenseesRender(true);
+      setAddFloorAndPlacementTypesRender(false);
+      setUpdateShippingCostsRender(false);
+      setUpdateMaterialCostsAndInventoryRender(false);
+      setSystemAdminRender(false);
+    } else if (selectedPage == 2) {
+      setUpdateLicenseesRender(false);
+      setAddFloorAndPlacementTypesRender(true);
+      setUpdateShippingCostsRender(false);
+      setUpdateMaterialCostsAndInventoryRender(false);
+      setSystemAdminRender(false);
+    } else if (selectedPage == 3) {
+      setUpdateLicenseesRender(false);
+      setAddFloorAndPlacementTypesRender(false);
+      setUpdateShippingCostsRender(true);
+      setUpdateMaterialCostsAndInventoryRender(false);
+      setSystemAdminRender(false);
+    } else if (selectedPage == 4) {
+      setUpdateLicenseesRender(false);
+      setAddFloorAndPlacementTypesRender(false);
+      setUpdateShippingCostsRender(false);
+      setUpdateMaterialCostsAndInventoryRender(true);
+      setSystemAdminRender(false);
+    } else if (selectedPage == 5) {
+      setUpdateLicenseesRender(false);
+      setAddFloorAndPlacementTypesRender(false);
+      setUpdateShippingCostsRender(false);
+      setUpdateMaterialCostsAndInventoryRender(false);
+      setSystemAdminRender(true);
+    }
     
   }
 
