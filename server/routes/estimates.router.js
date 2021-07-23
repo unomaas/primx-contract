b7b6eab3fae90643dc2d7bd0e389b317a8718393
Brandon Wharton <whartonbm@gmail.com>
2021-07-23 16:54:02 -0500

    Made a GET route for all estimates GET to be used in admin estimates data grid

--- server/routes/estimates.router.js
+++ server/routes/estimates.router.js
@@ -9,6 +9,26 @@ const router = express.Router();
     // GET route code here
   });
   
+// GET request to get all estimates data from the database
+router.get('/all', (req, res) => {
+  // SQL query to GET all estimates along the floor type names, licensee names, placement type names, and shipping state/province names
+  const queryText = `SELECT "estimates".*, "floor_types".floor_type, "licensees".licensee_contractor_name, "placement_types".placement_type, "shipping_costs".ship_to_state_province FROM "estimates"
+                     JOIN "floor_types" ON "estimates".floor_types_id = "floor_types".id
+                     JOIN "licensees" ON "estimates".licensee_id = "licensees".id
+                     JOIN "placement_types" ON "estimates".placement_types_id = "placement_types".id
+                     JOIN "shipping_costs" ON "estimates".shipping_costs_id = "shipping_costs".id;`;
+  // DB request
+  pool.query(queryText)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('Error with /api/estimates/all GET: ', error);
+      res.sendStatus(500);
+    })
+                  
+})
+
   /**
    * POST route template
    */
