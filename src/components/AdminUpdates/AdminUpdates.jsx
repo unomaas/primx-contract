452253833fceb02a0c065ac92f2c61354a47587b
Alex Kim <alexkim251@yahoo.com>
2021-07-23 15:38:23 -0500

    added selection bar and heading for 3.0 home page for admin updates

--- src/components/AdminUpdates/AdminUpdates.jsx
+++ src/components/AdminUpdates/AdminUpdates.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Select from '@material-ui/core'
 
 export default function AdminUpdates() {
+
+  const handleSelectPage = (event) => {
+
+  }
+
+
   return (
     <div>
-      
+        <H2>Select Update Field</H2>
+        {/* drop down for page selection */}
+        <Select value={""} onchange={handleSelectPage}>
+          <option>Select Page</option>
+          <option value={1}>Update Licensees</option>
+          <option value={2}>Add Floor Types/Placement Types</option>
+          <option value={3}>Update Shipping Costs</option>
+          <option value={4} >Update Material Costs/Inventory</option>
+          {/* need to add conditional render for super user for system admin */}
+          <option value={5}>System Admin</option>
+        </Select>
     </div>
   )
 }
