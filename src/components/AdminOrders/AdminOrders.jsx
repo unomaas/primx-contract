3d862d852b90c61e6215125b7a8bd7304264038b
Brandon Wharton <whartonbm@gmail.com>
2021-07-24 11:52:21 -0500

    Brought Typography into AdminOrders to make headers, created an AdminEstimatesGrid component to populate the DataGrid

--- src/components/AdminOrders/AdminOrders.jsx
+++ src/components/AdminOrders/AdminOrders.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+// Material-UI components
+import Typography from '@material-ui/core/Typography';
+// components
+import AdminEstimatesGrid from '../AdminEstimatesGrid/AdminEstimatesGrid';
 
 export default function AdminOrders() {
   const dispatch = useDispatch();
@@ -16,7 +20,18 @@ export default function AdminOrders() {
   console.log('estimates array:', estimatesArray);
   return (
     <div>
-      
+      <Typography variant="h3" component="h2" align="center">
+        Pending Orders
+      </Typography>
+      {/* Pending data grid here */}
+      <Typography variant="h3" component="h2" align="center">
+        Processed Orders
+      </Typography>
+      {/* Processed data grid here */}
+      <Typography variant="h3" component="h2" align="center">
+        Open Estimates
+      </Typography>
+      <AdminEstimatesGrid estimatesArray={estimatesArray} />
     </div>
   )
 }
