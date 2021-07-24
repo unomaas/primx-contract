e2a9bc971a73b519f65b8d80cf3a11e22b2f2b3c
Brandon Wharton <whartonbm@gmail.com>
2021-07-24 11:00:57 -0500

    Brought dispatch and useEffect into AdminOrders, made a protected route at /adminorders

--- src/components/AdminOrders/AdminOrders.jsx
+++ src/components/AdminOrders/AdminOrders.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux'
 
 export default function AdminOrders() {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    // GET all estimates data on page load
+    dispatch({type: 'FETCH_ALL_ESTIMATES' });
+  }, [])
+
+
   return (
     <div>
       
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -89,6 +89,9 @@ function App() {
               <LicenseeHomePage />
             </ProtectedRoute>
 
+            <ProtectedRoute exact path="/adminorders">
+              <AdminOrders />
+            </ProtectedRoute>
             {/* If none of the other routes matched, we will show a 404. */}
             <Route>
               <h1>404</h1>
