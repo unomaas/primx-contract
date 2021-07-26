ebc08367c982901eb4e902fb1eb78095f08a1e8b
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-26 15:35:16 -0500

    made backend for company get route

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -9,6 +9,23 @@ const router = express.Router();
     // GET route code here
   });
   
+
+// GET Request to grab all Liscensee/Company Name from DB for View 1.0
+router.get('/company', (req, res) => {
+  const queryText= 'SELECT * FROM table "licensees";';
+   
+  // DB request
+   pool.query(queryText)
+   .then(result => {
+     res.send(result.rows);
+   })
+   .catch(error => {
+     console.log('Error with /api/admin/company GET: ', error);
+     res.sendStatus(500);
+   })
+});
+
+
   /**
    * POST route template
    */
