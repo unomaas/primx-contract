a282d93f2ee6cb3a7dad3709c1618c58973e4e6e
xiongster94 <xiongster94@users.noreply.github.com>
2021-07-26 15:53:56 -0500

    made all routes for 1.0 view field inputs

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -9,10 +9,11 @@ const router = express.Router();
     // GET route code here
   });
   
+/***** ⬇⬇ Liscensee Form GET Requests ⬇⬇ *****/
 
 // GET Request to grab all Liscensee/Company Name from DB for View 1.0
 router.get('/company', (req, res) => {
-  const queryText= 'SELECT * FROM table "licensees";';
+  const queryText= `SELECT * FROM "licensees";`;
    
   // DB request
    pool.query(queryText)
@@ -25,6 +26,52 @@ router.get('/company', (req, res) => {
    })
 });
 
+// GET Request to grab all States/Province from DB for View 1.0
+router.get('/state', (req, res) => {
+  const queryText= `SELECT 'ship_to_state_province' FROM "shipping_costs";`;
+   
+  // DB request
+   pool.query(queryText)
+   .then(result => {
+     res.send(result.rows);
+   })
+   .catch(error => {
+     console.log('Error with /api/admin/state GET: ', error);
+     res.sendStatus(500);
+   })
+});
+
+// GET Request to grab all floor types from DB for View 1.0
+router.get('/floor', (req, res) => {
+  const queryText= `SELECT * FROM "floor_types";`;
+   
+  // DB request
+   pool.query(queryText)
+   .then(result => {
+     res.send(result.rows);
+   })
+   .catch(error => {
+     console.log('Error with /api/admin/floor GET: ', error);
+     res.sendStatus(500);
+   })
+});
+
+// GET Request to grab all placement types from DB for View 1.0
+router.get('/placement', (req, res) => {
+  const queryText= `SELECT * FROM "placement_types";`;
+   
+  // DB request
+   pool.query(queryText)
+   .then(result => {
+     res.send(result.rows);
+   })
+   .catch(error => {
+     console.log('Error with /api/admin/placement GET: ', error);
+     res.sendStatus(500);
+   })
+});
+
+/***** ⬆⬆ Liscensee Form GET Requests ⬆⬆ *****/ 
 
   /**
    * POST route template
