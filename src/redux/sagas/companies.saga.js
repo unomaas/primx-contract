70b5099519faaf4402439ef1c93d457c084c4775
Alex Kim <alexkim251@yahoo.com>
2021-07-26 11:06:49 -0500

    added companies saga to root saga

--- src/redux/sagas/companies.saga.js
+++ src/redux/sagas/companies.saga.js
@@ -1,18 +1,18 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-// worker Saga: will be fired on "FETCH_USER" actions
+// fetchCompanies generator to use get from the db
 function* fetchCompanies() {
     try {
       
       const response = yield axios.get('/api/companies');
-
+        // set companies used in reducer
       yield put({ type: 'SET_COMPANIES', payload: response.data });
     } catch (error) {
       console.log('User get request failed', error);
     }
   }
-  
+  // companies saga to fetch companies
   function* companiesSaga() {
     yield takeLatest('FETCH_COMPANIES', fetchCompanies);
   }
