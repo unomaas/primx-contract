fbe0a5a77329525deef07639dd5a2afa523385a8
Chris Klemz <crklemz@gmail.com>
2021-07-27 11:52:04 -0500

    added sweetalert for adding new admin

--- src/components/AdminRegisterForm/AdminRegisterForm.jsx
+++ src/components/AdminRegisterForm/AdminRegisterForm.jsx
@@ -7,6 +7,7 @@ import { useHistory } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import swal from 'sweetalert';
 
 
 
@@ -40,7 +41,10 @@ function AdminRegisterForm() {
         password: password,
       },
     });
-    history.push('/user')
+    swal("Success!", "New Admin Created", "success", {
+      button: "OK",
+    });
+    history.push('/user');
   }; // end registerUser
 
   return (
