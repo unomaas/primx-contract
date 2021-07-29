0c2ff517336392fa43e9dea0d1c95f940751a334
Ryan Maas <rdmjobs@live.com>
2021-07-28 19:55:21 -0500

    Pulled main, merging into estimatecreate.

---
+++ src/components/ButtonToggle/ButtonToggle.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+//imports for MUI
+import { useStyles } from '../MuiStyling/MuiStyling';
+import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function ButtonToggle() {
+
+  const history = useHistory();
+  const buttonState = useSelector(store => store.estimatesReducer.buttonState);
+
+  // toggle button states
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const handleButtonState = (event, selection) => {
+    console.log('In handleButtonState, selection:', selection);
+    if (selection !== null) {
+      dispatch({
+        type: 'SET_BUTTON_STATE',
+        payload: selection
+      });
+      history.push(`/${selection}`);
+
+    }
+  }
+
+  return (
+    <div className="">
+      <ToggleButtonGroup
+        exclusive
+        onChange={handleButtonState}
+        value={buttonState}
+        size="small"
+      >
+        <ToggleButton
+          style={{ fontFamily: 'Lexend Tera', fontSize: '11px' }}
+          value="create"
+        >
+          Create New Estimate
+        </ToggleButton>
+        <ToggleButton
+          style={{ fontFamily: 'Lexend Tera', fontSize: '11px' }}
+          value="lookup"
+        >
+          Search For Estimate
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
+};
+
+
