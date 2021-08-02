6c50fba8cffeaea618545629aef1d2ffad10fd70
Ryan Maas <rdmjobs@live.com>
2021-08-02 13:59:28 -0500

    Merging with main.

--- src/components/AdminUpdates/AdminUpdateLicenses.jsx
+++ src/components/AdminUpdates/AdminUpdateLicenses.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AdminUpdates from './AdminUpdates';
 
@@ -21,21 +21,21 @@ export default function AdminUpdateLicenses() {
   // establish companies with a use selector from the companies reducer
   const companies = useSelector(store => store.companies);
   // establish add company input state with use state
-  let [companyNameInput,setCompanyNameInput] = useState('');
+  let [companyNameInput, setCompanyNameInput] = useState('');
   // establish snackbar variables for notifications
   const snack = useSelector(store => store.snackBar);
-  
 
-     //styles for MUI
-     const useStyles = makeStyles((theme) => ({
-      root: {
-        '& > *': {
-          margin: theme.spacing(1),
-        },
+
+  //styles for MUI
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
       },
-    }));
-    //defining classes for MUI
-    const classes = useStyles();
+    },
+  }));
+  //defining classes for MUI
+  const classes = useStyles();
 
 
   //establish rows with campanies array for datagrid
@@ -64,12 +64,12 @@ export default function AdminUpdateLicenses() {
   }
   //handles add company button click that sends payload of company name input to saga for posting to database
   const handleAddCompany = (event) => {
-    if(companyNameInput == '') {
-      dispatch({type: 'SET_EMPTY_ERROR'})
+    if (companyNameInput == '') {
+      dispatch({ type: 'SET_EMPTY_ERROR' })
     } else {
-      dispatch({type: 'ADD_COMPANY', payload: companyNameInput});
+      dispatch({ type: 'ADD_COMPANY', payload: companyNameInput });
       setCompanyNameInput('');
-      
+
     }
 
   }
@@ -79,35 +79,35 @@ export default function AdminUpdateLicenses() {
       return;
     }
 
-    dispatch({type: 'SET_CLOSE'})
+    dispatch({ type: 'SET_CLOSE' })
   };
 
 
-return (
-  <div >
-    <AdminUpdates />
-    <h2>Update Licensee</h2>
-    <TextField id="outlined-basic" label="Add New Licensee" variant="outlined" value={companyNameInput} onChange={handleCompanyInputChange}/>
-    <Fab className={classes.root} onClick={handleAddCompany} color="primary" aria-label="add">
-              <AddIcon />
-            </Fab>
-    <div style={{ height: 350, width: '28%' }}
-      className="AdminEstimatesGrid-wrapper">
-      <DataGrid
-        style={{ fontFamily: 'Times New Roman', fontSize: '1em' }}
-        rows={rows}
-        columns={columns}
-        pageSize={10}
-        onEditCellChangeCommitted={handleEditSubmit}
-      />
-      <Snackbar open={snack.open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity={snack.severity}>
-          {snack.message}
-        </Alert>
-      </Snackbar>
+  return (
+    <div >
+      <AdminUpdates />
+      <h2>Update Licensee</h2>
+      <TextField id="outlined-basic" label="Add New Licensee" variant="outlined" value={companyNameInput} onChange={handleCompanyInputChange} />
+      <Fab className={classes.root} onClick={handleAddCompany} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <div style={{ height: 350, width: '28%' }}
+        className="AdminEstimatesGrid-wrapper">
+        <DataGrid
+          style={{ fontFamily: 'Times New Roman', fontSize: '1em' }}
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          onEditCellChangeCommitted={handleEditSubmit}
+        />
+        <Snackbar open={snack.open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity={snack.severity}>
+            {snack.message}
+          </Alert>
+        </Snackbar>
+      </div>
     </div>
-  </div>
 
 
-)
+  )
 }
