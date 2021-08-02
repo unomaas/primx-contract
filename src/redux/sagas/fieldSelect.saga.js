7621e238b0dcd71f05b63db92c5230b20742fffe
xiongster94 <xiongster94@users.noreply.github.com>
2021-08-02 08:25:32 -0500

    added saga for all field select menu

---
+++ src/redux/sagas/fieldSelect.saga.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// NEED SHIPPINGCOSTS
+
+// NEED FLOOR TYPE
+
+// NEED PLACEMENT TYPYE
+
+// NEED COMPANY NAME
+
+// NEED STATE
