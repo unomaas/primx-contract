e24c21bc63c524a8b120857103082ac365820ca3
Chris Klemz <crklemz@gmail.com>
2021-08-09 15:00:09 -0500

    chris clean up code complete

--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+import {
+  put,
+  takeLatest
+} from 'redux-saga/effects';
 
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchUser() {
   try {
     const config = {
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json'
+      },
       withCredentials: true,
     };
-
 
     // the config includes credentials which
     // allow the server session to recognize the user
@@ -16,11 +20,13 @@ function* fetchUser() {
     // from the server session (req.user)    
     const response = yield axios.get('/api/user', config);
 
-
     // now that the session has given us a user object
     // with an id and username set the client-side user object to let
     // the client-side code know the user is logged in
-    yield put({ type: 'SET_USER', payload: response.data });
+    yield put({
+      type: 'SET_USER',
+      payload: response.data
+    });
 
   } catch (error) {
     console.log('User get request failed', error);
