f406d3972f62f6cd4d6631e7685b5bb97240253c
Chris Klemz <crklemz@gmail.com>
2021-08-09 14:42:01 -0500

    nav file code cleaned up

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,11 +4,8 @@ import './Nav.css';
 import NavDrawer from '../NavDrawer/NavDrawer';
 // ⬇ Dependent functionality:
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
-import MenuIcon from '@material-ui/icons/Menu';
-import { SwipeableDrawer, Button, List, ListItem, ListItemText, Divider, IconButton, Box, Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 
 //#endregion ⬆⬆ Document setup above. 
 
@@ -17,7 +14,6 @@ function Nav() {
   //#region ⬇⬇ All state variables below:
   const user = useSelector((store) => store.user);
   //#endregion ⬆⬆ All state variables above. 
-
 
   let loginLinkData = {
     path: '/login',
@@ -39,48 +35,6 @@ function Nav() {
           alt="PrimX Company Logo"
         />
       </Box>
-
-      <div className='PackMeUpImage-wrapper'>
-        {/* ⬇ Conditioning rendering to switch logos depending on login:
-        {user.id ? (
-          // ⬇If the user is logged in, show full logo:
-          <Link to='/dashboard'>
-            <img
-              className='PackMeUpImage'
-              src="./images/PMUWhiteReg-01.svg"
-              alt="Pack Me Up Logo"
-            />
-          </Link>
-        ) : (
-          // ⬇ If not logged in, show the icon logo: 
-          <Link to='/home'>
-            <img
-              className='PackMeUpImage'
-              src="./images/PMUWhiteIcon.svg"
-              alt="Pack Me Up Logo"
-            />
-          </Link>
-        )} */}
-      </div>
-
-      {/* <div>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
-
-        {user.id && (
-          <>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div> */}
 
       <div className='NavDrawer-wrapper'>
         <NavDrawer className="NavDrawer-icon" />
