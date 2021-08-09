be760486ea54d26ba5a9ae1cf0bf238e6cd147b8
Chris Klemz <crklemz@gmail.com>
2021-08-09 13:42:15 -0500

    cleaned up system admin page

--- src/components/AdminUpdates/SystemAdmin.jsx
+++ src/components/AdminUpdates/SystemAdmin.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 
-
 //import components
 import AdminUpdates from './AdminUpdates';
 import SystemAdminUpdateUsersGrid from './SystemAdminUpdateUsersGrid';
 import AdminRegisterForm from '../AdminRegisterForm/AdminRegisterForm';
 
-
 export default function SystemAdmin() {
 
-  
+
   return (
     <div>
-
       <div>
         {/* shows nav dropdown */}
         <AdminUpdates />
       </div>
-      
+
       <div>
         <AdminRegisterForm />
       </div>
 
+      {/* providing some spacing */}
       <br></br>
       <br></br>
 
@@ -29,7 +27,6 @@ export default function SystemAdmin() {
         {/* grid component where all of the info and work is happening */}
         <SystemAdminUpdateUsersGrid />
       </div>
-
     </div>
   )
 }
