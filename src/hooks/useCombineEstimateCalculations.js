3b2fffb4efc10d20e3239aabd0145a21bb2c9597
Ryan Maas <rdmjobs@live.com>
2021-11-10 20:01:04 -0600

    Resolving the combine troubles.

--- src/hooks/useCombineEstimateCalculations.js
+++ src/hooks/useCombineEstimateCalculations.js
@@ -1,5 +1,5 @@
 // Custom hook to take in an estimate object and return a mutated object with new keys based on the necessary math needed for all the displays
-export default function useEstimateCalculations(estimate) {
+export default function useCombineEstimateCalculations(estimate) {
   // set a default value for waste factor percentage if one wasn't entered
   if (!estimate.waste_factor_percentage) {
     estimate.waste_factor_percentage = 5;
@@ -14,16 +14,16 @@ export default function useEstimateCalculations(estimate) {
     estimate.primx_cpea_shipping_estimate = estimate.flow_cpea_price;
   }
 
-  // This block checks to see if a price estimate exists already, indicating that the calculations are being done on the edit view between Estimate
-  // Lookup and EstimateCreate. estimate.dc_price only exists if a new shipping state has been selected from the dropdown menu in EstimateCreate.
-  // If both these conditions are met, it means a user is editing an estimate and is changing their desired shipping state. When this happens,
-  // set the shipping estimate keys to match the new keys set by the change in ship-to state or province.
-  if (estimate.design_total_price_estimate && estimate.dc_price) {
-    estimate.primx_dc_shipping_estimate = estimate.dc_price;
-    estimate.primx_flow_shipping_estimate = estimate.flow_cpea_price;
-    estimate.primx_steel_fibers_shipping_estimate = estimate.fibers_price;
-    estimate.primx_cpea_shipping_estimate = estimate.flow_cpea_price;
-  }
+  // // This block checks to see if a price estimate exists already, indicating that the calculations are being done on the edit view between Estimate
+  // // Lookup and EstimateCreate. estimate.dc_price only exists if a new shipping state has been selected from the dropdown menu in EstimateCreate.
+  // // If both these conditions are met, it means a user is editing an estimate and is changing their desired shipping state. When this happens,
+  // // set the shipping estimate keys to match the new keys set by the change in ship-to state or province.
+  // if (estimate.design_total_price_estimate && estimate.dc_price) {
+  //   estimate.primx_dc_shipping_estimate = estimate.dc_price;
+  //   estimate.primx_flow_shipping_estimate = estimate.flow_cpea_price;
+  //   estimate.primx_steel_fibers_shipping_estimate = estimate.fibers_price;
+  //   estimate.primx_cpea_shipping_estimate = estimate.flow_cpea_price;
+  // }
 
 
   // start by running a loop on the entire object and removing dollar signs and commas from all money quantities from database
@@ -42,24 +42,24 @@ export default function useEstimateCalculations(estimate) {
 
   // imperial calculalations
   if (estimate.measurement_units == 'imperial') {
-    // cubic yards base: original formula given to us for cubic yards divided quantity first by 12, then by 27
-    estimate.cubic_yards = (estimate.square_feet * estimate.thickness_inches / 324);
-    // additional thickness for thickened edges
-    if (estimate.thickness_inches >= 6) { // if greater than 6 inches, no need for thickened edges
-      estimate.additional_thickness_inches = 0;
-    } else {
-      estimate.additional_thickness_inches = ((6 - estimate.thickness_inches) * .5);
-    }
-    // calculate perimeter thickening and construction joint thickening based on calculated additional thickness, the 5 and 10 values below are provided by PrimX
-    estimate.perimeter_thickening_cubic_yards = (estimate.thickened_edge_perimeter_lineal_feet * 5) * (estimate.additional_thickness_inches / 324);
-    estimate.construction_joint_thickening_cubic_yards = (estimate.thickened_edge_construction_joint_lineal_feet * 10) * (estimate.additional_thickness_inches / 324);
-    // calculate subtotal based on above results, factor in waste factor percentage, and calculate the total design cubic yards
-    estimate.cubic_yards_subtotal = estimate.cubic_yards + estimate.perimeter_thickening_cubic_yards + estimate.construction_joint_thickening_cubic_yards;
-    estimate.waste_factor_cubic_yards = estimate.cubic_yards_subtotal * (estimate.waste_factor_percentage / 100);
-    estimate.design_cubic_yards_total = estimate.cubic_yards_subtotal + estimate.waste_factor_cubic_yards;
+    // // cubic yards base: original formula given to us for cubic yards divided quantity first by 12, then by 27
+    // estimate.cubic_yards = (estimate.square_feet * estimate.thickness_inches / 324);
+    // // additional thickness for thickened edges
+    // if (estimate.thickness_inches >= 6) { // if greater than 6 inches, no need for thickened edges
+    //   estimate.additional_thickness_inches = 0;
+    // } else {
+    //   estimate.additional_thickness_inches = ((6 - estimate.thickness_inches) * .5);
+    // }
+    // // calculate perimeter thickening and construction joint thickening based on calculated additional thickness, the 5 and 10 values below are provided by PrimX
+    // estimate.perimeter_thickening_cubic_yards = (estimate.thickened_edge_perimeter_lineal_feet * 5) * (estimate.additional_thickness_inches / 324);
+    // estimate.construction_joint_thickening_cubic_yards = (estimate.thickened_edge_construction_joint_lineal_feet * 10) * (estimate.additional_thickness_inches / 324);
+    // // calculate subtotal based on above results, factor in waste factor percentage, and calculate the total design cubic yards
+    // estimate.cubic_yards_subtotal = estimate.cubic_yards + estimate.perimeter_thickening_cubic_yards + estimate.construction_joint_thickening_cubic_yards;
+    // estimate.waste_factor_cubic_yards = estimate.cubic_yards_subtotal * (estimate.waste_factor_percentage / 100);
+    // estimate.design_cubic_yards_total = estimate.cubic_yards_subtotal + estimate.waste_factor_cubic_yards;
 
-    // calculate amounts and prices of materials that are measured in pounds and square feet, start with DC
-    estimate.primx_dc_total_amount_needed = estimate.design_cubic_yards_total * 67; // 67 is the factor provided by PrimX
+    // // calculate amounts and prices of materials that are measured in pounds and square feet, start with DC
+    // estimate.primx_dc_total_amount_needed = estimate.design_cubic_yards_total * 67; // 67 is the factor provided by PrimX
     estimate.primx_dc_packages_needed = Math.ceil(estimate.primx_dc_total_amount_needed / 2756); // dc comes in packages of 2756 lbs, need to round up
     estimate.primx_dc_total_order_quantity = estimate.primx_dc_packages_needed * 2756;
     estimate.primx_dc_total_materials_price = estimate.primx_dc_total_order_quantity * estimate.primx_dc_unit_price;
@@ -69,7 +69,7 @@ export default function useEstimateCalculations(estimate) {
     estimate.primx_dc_total_cost_estimate = estimate.primx_dc_calculated_shipping_estimate + estimate.primx_dc_total_materials_price;
 
     // calculate values for PrimX steel fibers
-    estimate.primx_steel_fibers_total_amount_needed = estimate.design_cubic_yards_total * estimate.primx_steel_fibers_dosage_lbs;
+    // estimate.primx_steel_fibers_total_amount_needed = estimate.design_cubic_yards_total * estimate.primx_steel_fibers_dosage_lbs;
     // steel fibers comes in packages of 42329 lbs, need to round up
     estimate.primx_steel_fibers_packages_needed = Math.ceil(estimate.primx_steel_fibers_total_amount_needed / 42329);
     estimate.primx_steel_fibers_total_order_quantity = estimate.primx_steel_fibers_packages_needed * 42329;
@@ -80,7 +80,7 @@ export default function useEstimateCalculations(estimate) {
     estimate.primx_steel_fibers_total_cost_estimate = estimate.primx_steel_fibers_calculated_shipping_estimate + estimate.primx_steel_fibers_total_materials_price;
 
     // calculate values for PrimX Ultracure Blankets
-    estimate.primx_ultracure_blankets_total_amount_needed = estimate.square_feet * 1.2; // 1.2 is the factor provided by PrimX
+    // estimate.primx_ultracure_blankets_total_amount_needed = estimate.square_feet * 1.2; // 1.2 is the factor provided by PrimX
     // blankets come in rolls of 6458 sq feet, need to round up
     estimate.primx_ultracure_blankets_packages_needed = Math.ceil(estimate.primx_ultracure_blankets_total_amount_needed / 6458);
     estimate.primx_ultracure_blankets_total_order_quantity = estimate.primx_ultracure_blankets_packages_needed * 6458;
@@ -149,7 +149,7 @@ export default function useEstimateCalculations(estimate) {
   // at. Works for both metric and imperial units since PrimX Flow and CPEA are measured in liters no matter what.
   function calculateFlowAndCpea(designVolume) {
     // start by adding calculations data for PrimX Flow
-    estimate.primx_flow_total_amount_needed = designVolume * estimate.primx_flow_dosage_liters; // Dosage liters is input by licensee
+    // estimate.primx_flow_total_amount_needed = designVolume * estimate.primx_flow_dosage_liters; // Dosage liters is input by licensee
     // flow comes in packages of 1000 liters, need to round up
     estimate.primx_flow_packages_needed = Math.ceil(estimate.primx_flow_total_amount_needed / 1000);
     estimate.primx_flow_total_order_quantity = estimate.primx_flow_packages_needed * 1000;
@@ -161,7 +161,7 @@ export default function useEstimateCalculations(estimate) {
 
 
     // add calculations data for PrimX CPEA
-    estimate.primx_cpea_total_amount_needed = designVolume * estimate.primx_cpea_dosage_liters; // Dosage liters is input by licensee
+    // estimate.primx_cpea_total_amount_needed = designVolume * estimate.primx_cpea_dosage_liters; // Dosage liters is input by licensee
     // cpea comes in packages of 1000 liters, need to round up
     estimate.primx_cpea_packages_needed = Math.ceil(estimate.primx_cpea_total_amount_needed / 1000);
     estimate.primx_cpea_total_order_quantity = estimate.primx_cpea_packages_needed * 1000;
