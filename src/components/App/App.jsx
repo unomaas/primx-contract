3d9dc97ea8b119e92f918ffbafcc78d6e3435611
Ryan Maas <rdmjobs@live.com>
2021-11-23 20:18:10 -0600

    Loading screen updated>

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,146 +33,150 @@ function App() {
   //#region ⬇⬇ All state variables below:
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
-  const backdropState = useSelector(store => store.backdropReducer);
-
+  const backdropState = useSelector(store => store.backdropReducer.backdropReducer);
+  const isLoading = useSelector(store => store.backdropReducer.isLoading)
   // ⬇ Runs on page load:
   useEffect(() => {
     dispatch({ type: 'FETCH_USER' })
   }, [dispatch]);
   //#endregion ⬆⬆ All state variables above. 
 
-  // ⬇  This manages the backdrop and loading screen. If true, show header/loader:
-  if (backdropState) {
-    return <div className="App">
-      <Nav />
-      <MuiBackdropManager />
-      {/* <Footer /> */}
-    </div>;
-  }
+  // // ⬇  This manages the backdrop and loading screen. If true, show header/loader:
+  // if (backdropState) {
+  //   console.log('*** backdropState is:', backdropState);
+  //   return <MuiBackdropManager />;
+  // }
 
   // ⬇ Rendering:
+  console.log('*** is loading :', isLoading);
   return (
     <ThemeProvider theme={theme}>
       <Router>
+        <MuiBackdropManager />
+
         <div className="App">
+
 
           {/* This manages the Snackbar alerts throughout the app: */}
           <MuiSnackbarManager />
 
+
           <Nav />
 
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/create" />
-            <Redirect exact from="/home" to="/create" />
+          <div className="ContentWrapper" style={{display: `${isLoading}`}}>
+            <Switch>
+              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+              <Redirect exact from="/" to="/create" />
+              <Redirect exact from="/home" to="/create" />
 
-            {/* Home/Landing Page with Create New or Lookup Estimate: */}
-            <Route exact path="/create">
-              <EstimateCreate />
-            </Route>
+              {/* Home/Landing Page with Create New or Lookup Estimate: */}
+              <Route exact path="/create">
+                <EstimateCreate />
+              </Route>
 
-            {/* /lookup leads to the search estimate view for finding individual estimates */}
-            <Route exact path="/lookup">
-              <EstimateLookup />
-            </Route>
+              {/* /lookup leads to the search estimate view for finding individual estimates */}
+              <Route exact path="/lookup">
+                <EstimateLookup />
+              </Route>
 
-            {/* This route bring user to specific estimate in /lookup */}
-            <Route
-              exact path="/lookup/:licensee_id_searched/:estimate_number_searched"
-              children={<EstimateLookup />}
-            >
-            </Route>
+              {/* This route bring user to specific estimate in /lookup */}
+              <Route
+                exact path="/lookup/:licensee_id_searched/:estimate_number_searched"
+                children={<EstimateLookup />}
+              >
+              </Route>
 
-            {/* This route bring user to specific estimate in /lookup */}
-            {/* <Route
+              {/* This route bring user to specific estimate in /lookup */}
+              {/* <Route
               exact path="/lookup/:licensee_id_searched/:estimate_number_searched/:first_estimate_number_combined/:second_estimate_number_combined"
               children={<EstimateLookup />}
             >
             </Route> */}
 
-            {/* This route bring user to specific estimate in /lookup */}
-            {/* <Route
+              {/* This route bring user to specific estimate in /lookup */}
+              {/* <Route
               exact path="/lookup/:licensee_id_searched/:estimate_number_searched/:first_estimate_number_combined/:second_estimate_number_combined/:third_estimate_number_combined"
               children={<EstimateLookup />}
             >
             </Route> */}
 
-            {/* /combine leads to the combine estimate view for combining individual estimates */}
-            <Route exact path="/combine">
-              <EstimateCombine />
-            </Route>
+              {/* /combine leads to the combine estimate view for combining individual estimates */}
+              <Route exact path="/combine">
+                <EstimateCombine />
+              </Route>
 
-            {/* (For Combinations of 2) This route bring user to specific estimate combinations in /combine */}
-            <Route
-              exact path="/combine/:licensee_id_searched/:first_estimate_number_combined/:second_estimate_number_combined"
-              children={<EstimateCombine />}
-            >
-            </Route>
+              {/* (For Combinations of 2) This route bring user to specific estimate combinations in /combine */}
+              <Route
+                exact path="/combine/:licensee_id_searched/:first_estimate_number_combined/:second_estimate_number_combined"
+                children={<EstimateCombine />}
+              >
+              </Route>
 
-            {/* (For Combinations of 3) This route bring user to specific estimate combinations in /combine */}
-            <Route
-              exact path="/combine/:licensee_id_searched/:first_estimate_number_combined/:second_estimate_number_combined/:third_estimate_number_combined"
-              children={<EstimateCombine />}
-            >
-            </Route>
+              {/* (For Combinations of 3) This route bring user to specific estimate combinations in /combine */}
+              <Route
+                exact path="/combine/:licensee_id_searched/:first_estimate_number_combined/:second_estimate_number_combined/:third_estimate_number_combined"
+                children={<EstimateCombine />}
+              >
+              </Route>
 
-            <Route exact path="/login" >
-              <AdminLoginPage />
-            </Route>
+              <Route exact path="/login" >
+                <AdminLoginPage />
+              </Route>
 
-            {/* For protected routes, the view could show one of several things on the same route.  Visiting localhost:3000/user will show the AdminLandingPage if the user is logged in.  If the user is not logged in, the ProtectedRoute will show the LoginPage (component).  Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-            {/* // logged in shows AdminLandingPage else shows LoginPage */}
-            <ProtectedRoute exact path="/user" >
-              <AdminLandingPage />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminUpdates" >
-              <AdminUpdates />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminOrders" >
-              <AdminOrders />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminUpdateTypes" >
-              <AdminUpdateTypes />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminUpdateLicenses" >
-              <AdminUpdateLicenses />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminUpdateMaterials" >
-              <AdminUpdateMaterials />
-            </ProtectedRoute>
-
-            <ProtectedRoute exact path="/AdminUpdateShipping" >
-              <AdminUpdateShipping />
-            </ProtectedRoute>
-
-            {/* If logged in and user id is 1, that makes the super-admin and allows them to see this system admin page */}
-            {user.id == '1' ?
-              <ProtectedRoute exact path="/SystemAdmin" >
-                <SystemAdmin />
+              {/* For protected routes, the view could show one of several things on the same route.  Visiting localhost:3000/user will show the AdminLandingPage if the user is logged in.  If the user is not logged in, the ProtectedRoute will show the LoginPage (component).  Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+              {/* // logged in shows AdminLandingPage else shows LoginPage */}
+              <ProtectedRoute exact path="/user" >
+                <AdminLandingPage />
               </ProtectedRoute>
-              : <>
+
+              <ProtectedRoute exact path="/AdminUpdates" >
+                <AdminUpdates />
+              </ProtectedRoute>
+
+              <ProtectedRoute exact path="/AdminOrders" >
+                <AdminOrders />
+              </ProtectedRoute>
+
+              <ProtectedRoute exact path="/AdminUpdateTypes" >
+                <AdminUpdateTypes />
+              </ProtectedRoute>
+
+              <ProtectedRoute exact path="/AdminUpdateLicenses" >
+                <AdminUpdateLicenses />
+              </ProtectedRoute>
+
+              <ProtectedRoute exact path="/AdminUpdateMaterials" >
+                <AdminUpdateMaterials />
+              </ProtectedRoute>
+
+              <ProtectedRoute exact path="/AdminUpdateShipping" >
+                <AdminUpdateShipping />
+              </ProtectedRoute>
+
+              {/* If logged in and user id is 1, that makes the super-admin and allows them to see this system admin page */}
+              {user.id == '1' ?
+                <ProtectedRoute exact path="/SystemAdmin" >
+                  <SystemAdmin />
+                </ProtectedRoute>
+                : <>
+                  <h1>Error 404: Page Not Found.</h1>
+                  <h3>Please go back and try a different option.</h3>
+                </>}
+
+              <ProtectedRoute exact path="/adminorders">
+                <AdminOrders />
+              </ProtectedRoute>
+
+              {/* If none of the other routes matched, we will show a 404. */}
+              <Route>
                 <h1>Error 404: Page Not Found.</h1>
                 <h3>Please go back and try a different option.</h3>
-              </>}
+              </Route>
 
-            <ProtectedRoute exact path="/adminorders">
-              <AdminOrders />
-            </ProtectedRoute>
+            </Switch>
 
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route>
-              <h1>Error 404: Page Not Found.</h1>
-              <h3>Please go back and try a different option.</h3>
-            </Route>
-
-          </Switch>
-
-          <Footer />
+            <Footer />
+          </div>
 
         </div>
       </Router>
--- src/redux/reducers/backdrop.reducer.js
+++ src/redux/reducers/backdrop.reducer.js
@@ -1,4 +1,7 @@
-const backdropReducer = (state = true, action) => {
+import { combineReducers } from 'redux';
+
+// Set default to true to turn on: 
+const backdropReducer = (state = false, action) => {
   switch (action.type) {
     case 'LOADING_SCREEN_ON':
       return true;
@@ -9,4 +12,19 @@ const backdropReducer = (state = true, action) => {
   }
 };
 
-export default backdropReducer;
+// Set default to 'none' to turn on: 
+const isLoading = (state = '', action) => {
+  switch (action.type) {
+    case 'LOADING_SCREEN_ON':
+      return 'none';
+    case 'LOADING_SCREEN_OFF':
+      return "";
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  backdropReducer,
+  isLoading,
+});
