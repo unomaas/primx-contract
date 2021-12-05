6480d6ee81e1c417c3df444745e0d8ee86de5c30
Ryan Maas <rdmjobs@live.com>
2021-12-04 21:01:09 -0600

    rolling DB.

--- src/redux/sagas/licenseeForm.saga.js
+++ src/redux/sagas/licenseeForm.saga.js
@@ -55,13 +55,11 @@ function* AddEstimate(action) {
     const history = action.payload.history
     // Saving the response and action.payload to variables for easier reading:
     const returnedEstimate = response.data;
-    // If we just saved a combined estimate, do not push them, as they're already at the page we would push to: 
+    // If we just saved a combined estimate, 
     if (returnedEstimate.estimate_number.charAt(returnedEstimate.estimate_number.length - 1) === "C") {
       yield axios.put(`/api/estimates/usedincombine`, action.payload);
-      // return;
-    } else { // Otherwise they were just saving a single estimate and we do push:
-      yield history.push(`/lookup/${returnedEstimate.licensee_id}/${returnedEstimate.estimate_number}`);
     }
+    yield history.push(`/lookup/${returnedEstimate.licensee_id}/${returnedEstimate.estimate_number}`);
   } catch (error) {
     console.error('AddEstimate POST request failed', error);
   }
