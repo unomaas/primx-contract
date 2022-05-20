c47924a8f9812798c42206bbb868bb39c4b9c0b6
Ryan Maas <rdmjobs@live.com>
2022-05-20 14:07:37 -0500

    Tables fixed.

--- src/components/AdminUpdates/AdminUpdateTypes.jsx
+++ src/components/AdminUpdates/AdminUpdateTypes.jsx
@@ -14,125 +14,129 @@ import TextField from '@material-ui/core/TextField';
 
 export default function AdminUpdateTypes() {
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  //defining states for sending data to server
-  let [newFloorType, setNewFloorType] = useState('');
-  let [newPlacementType, setNewPlacementType] = useState('');
+	//defining states for sending data to server
+	let [newFloorType, setNewFloorType] = useState('');
+	let [newPlacementType, setNewPlacementType] = useState('');
 
 
-  //useSelector for array of floor types
-  const floorTypes = useSelector(store => store.floorTypes);
-  const placementTypes = useSelector(store => store.placementTypes);
+	//useSelector for array of floor types
+	const floorTypes = useSelector(store => store.floorTypes);
+	const placementTypes = useSelector(store => store.placementTypes);
 
-  //defining classes for MUI
-  const classes = useStyles();
+	//defining classes for MUI
+	const classes = useStyles();
 
-  //the following handle...change functions set the floor type and placement type useStates
-  const handleFloorChange = (event) => {
-    setNewFloorType(event.target.value);
-  }
-  const handlePlacementChange = (event) => {
-    setNewPlacementType(event.target.value);
-  }
+	//the following handle...change functions set the floor type and placement type useStates
+	const handleFloorChange = (event) => {
+		setNewFloorType(event.target.value);
+	}
+	const handlePlacementChange = (event) => {
+		setNewPlacementType(event.target.value);
+	}
 
-  const addFloorType = () => {
-    if (newFloorType == '') {
-      //error shows if a field is empty
-      dispatch({ type: 'SET_EMPTY_ERROR' })
-    } else {
-      //sends newFloorType to reducer to add floor type to DB
-      dispatch({ type: "ADD_FLOOR_TYPE", payload: { floor_type: newFloorType } })
-      //empty input field after it is submitted
-      setNewFloorType('')
-    }
-  }
+	const addFloorType = () => {
+		if (newFloorType == '') {
+			//error shows if a field is empty
+			dispatch({ type: 'SET_EMPTY_ERROR' })
+		} else {
+			//sends newFloorType to reducer to add floor type to DB
+			dispatch({ type: "ADD_FLOOR_TYPE", payload: { floor_type: newFloorType } })
+			//empty input field after it is submitted
+			setNewFloorType('')
+		}
+	}
 
-  const addPlacementType = () => {
-    if (newPlacementType == '') {
-      //error shows if a field is empty
-      dispatch({ type: 'SET_EMPTY_ERROR' })
-    } else {
-      //sends newPlacementType to reducer to add floor type to DB
-      dispatch({ type: "ADD_PLACEMENT_TYPE", payload: { placement_type: newPlacementType } })
-      //empty input field after it is submitted
-      setNewPlacementType('')
-    }
-  }
+	const addPlacementType = () => {
+		if (newPlacementType == '') {
+			//error shows if a field is empty
+			dispatch({ type: 'SET_EMPTY_ERROR' })
+		} else {
+			//sends newPlacementType to reducer to add floor type to DB
+			dispatch({ type: "ADD_PLACEMENT_TYPE", payload: { placement_type: newPlacementType } })
+			//empty input field after it is submitted
+			setNewPlacementType('')
+		}
+	}
 
-  useEffect(() => {
-    // GET floor and placement types data on page load
-    dispatch({ type: 'FETCH_FLOOR_TYPES' });
-    dispatch({ type: 'FETCH_PLACEMENT_TYPES' });
-  }, [])
+	useEffect(() => {
+		// GET floor and placement types data on page load
+		dispatch({ type: 'FETCH_FLOOR_TYPES' });
+		dispatch({ type: 'FETCH_PLACEMENT_TYPES' });
+	}, [])
 
-  return (
-    <div>
-      <AdminUpdates />
-      <h2>Update Floor Types</h2>
+	return (
+		<div>
+			<AdminUpdates />
+			<h2>Update Floor Types</h2>
 
-      {/* input and add button for adding new floor types */}
+			{/* input and add button for adding new floor types */}
 
-      <form onSubmit={addFloorType}
-        noValidate
-        autoComplete="off">
-        <TextField
-          className={classes.AddTypesInput}
-          inputProps={{
-            style: {
-              fontSize: 25,
-              height: 15
-            }
-          }}
-          id="outlined-basic"
-          label="Add New Floor Type"
-          variant="outlined"
-          value={newFloorType}
-          onChange={handleFloorChange} />
+			<form onSubmit={addFloorType}
+				noValidate
+				autoComplete="off">
+				<TextField
+					className={classes.AddTypesInput}
+					inputProps={{
+						style: {
+							fontSize: 25,
+							height: 15
+						}
+					}}
+					id="outlined-basic"
+					label="Add New Floor Type"
+					variant="outlined"
+					value={newFloorType}
+					onChange={handleFloorChange}
+				/>
 
-        <Fab
-          className={classes.AddTypesInput}
-          type="submit"
-          color="primary"
-          aria-label="add">
-          <AddIcon />
-        </Fab>
-      </form>
+				<Fab
+					className={classes.AddTypesInput}
+					type="submit"
+					color="primary"
+					aria-label="add"
+				>
+					<AddIcon />
+				</Fab>
+			</form>
 
-      {/* showing floor types */}
-      <UpdateFloorTypesGrid floorTypes={floorTypes} />
+			{/* showing floor types */}
+			<UpdateFloorTypesGrid floorTypes={floorTypes} />
 
-      <h2>Update Placement Types</h2>
-      {/* input and add button for adding new placement types */}
-      <div style={{ margin: '0 auto' }}>
-        <form
-          onSubmit={addPlacementType}
-          noValidate autoComplete="off">
-          <TextField
-            className={classes.AddTypesInput}
-            inputProps={{
-              style: {
-                fontSize: 25,
-                height: 15
-              }
-            }}
-            id="outlined-basic"
-            label="Add New Placement Type"
-            variant="outlined"
-            value={newPlacementType}
-            onChange={handlePlacementChange} />
+			<h2>Update Placement Types</h2>
+			{/* input and add button for adding new placement types */}
+			<div style={{ margin: '0 auto' }}>
+				<form
+					onSubmit={addPlacementType}
+					noValidate autoComplete="off">
+					<TextField
+						className={classes.AddTypesInput}
+						inputProps={{
+							style: {
+								fontSize: 25,
+								height: 15
+							}
+						}}
+						id="outlined-basic"
+						label="Add New Placement Type"
+						variant="outlined"
+						value={newPlacementType}
+						onChange={handlePlacementChange}
+					/>
 
-          <Fab
-            className={classes.AddTypesInput}
-            type="submit"
-            color="primary"
-            aria-label="add">
-            <AddIcon />
-          </Fab>
-        </form>
-      </div>
-      {/* showing placement types */}
-      <UpdatePlacementTypesGrid placementTypes={placementTypes} />
-    </div>
-  )
+					<Fab
+						className={classes.AddTypesInput}
+						type="submit"
+						color="primary"
+						aria-label="add"
+					>
+						<AddIcon />
+					</Fab>
+				</form>
+			</div>
+			{/* showing placement types */}
+			<UpdatePlacementTypesGrid placementTypes={placementTypes} />
+		</div>
+	)
 }
