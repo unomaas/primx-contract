1cecec18319a563cf67c77ce44865ce51d514ba3
Ryan Maas <rdmjobs@live.com>
2023-01-03 12:20:44 -0600

    Added custom pagination.

--- src/components/AdminUpdates/UpdateShippingCostsGrid.jsx
+++ src/components/AdminUpdates/UpdateShippingCostsGrid.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 // Material-UI components
 import { useStyles } from '../MuiStyling/MuiStyling';
 import { DataGrid, GridToolbar, GridToolbarContainer, GridToolbarExport, GridToolbarColumnsButton, GridToolbarFilterButton, GridToolbarDensitySelector } from '@material-ui/data-grid';
-import { Button, ButtonGroup, ClickAwayListener, Grow, Fade, Popper, MenuItem, MenuList, Paper, Menu, TextField } from '@material-ui/core';
+import { Button, ButtonGroup, ClickAwayListener, Grow, Fade, Popper, MenuItem, MenuList, Paper, Menu, TextField, TablePagination } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
@@ -16,13 +16,21 @@ export default function UpdateShippingCostsGrid() {
 	const dispatch = useDispatch();
 	const shippingCosts = useSelector(store => store.shippingCosts);
 	const shippingDestinations = useSelector(store => store.shippingDestinations);
-	const [stateFilter, setStateFilter] = useState('');
-
-
-	const [pageSize, setPageSize] = useState(8);
+	const [stateFilter, setStateFilter] = useState(null);
 	const [selectedRow, setSelectedRow] = useState(null);
+	const rowsPerPageOptions = [8, 16, 24, 48, 100];
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions[0]);
 
-	console.log(`Ryan Here: `, { selectedRow, shippingDestinations, stateFilter });
+	//rows are from the shipping costs reducer
+	let rows = [];
+	for (let row of shippingCosts) {
+		if (stateFilter && stateFilter.destination_name !== row.destination_name) continue;
+		rows.push(row);
+	}
+
+
+	const rowsToDisplay = rows.slice(page * rowsPerPage, page * rowsPerPage + parseInt(rowsPerPage));
 
 
 
@@ -82,20 +90,14 @@ export default function UpdateShippingCostsGrid() {
 	]; // End 
 
 
-	//rows are from the shipping costs reducer
-	let rows = [];
-	for (let row of shippingCosts) {
-		if (stateFilter && stateFilter.destination_name !== row.destination_name) continue;
-		rows.push(row);
-	}
 
-	// ! Ryan Here, setup a filter against the State selection later.
+
 
 
 	useEffect(() => {
 		// GET shipping cost data on page load
 		dispatch({ type: 'FETCH_SHIPPING_COSTS' }),
-		dispatch({ type: 'FETCH_SHIPPING_DESTINATIONS' })
+			dispatch({ type: 'FETCH_SHIPPING_DESTINATIONS' })
 	}, [])
 
 	// submit handler for in-line cell edits on the data grid
@@ -114,6 +116,11 @@ export default function UpdateShippingCostsGrid() {
 
 	// ⬇ A Custom Toolbar specifically made for the Shipping Costs Data Grid:
 	const CustomToolbar = () => {
+
+		const handleStateFilter = (value) => {
+			setStateFilter(value);
+			setPage(0);
+		}; // End handleStateFilter
 
 		const [anchorEl, setAnchorEl] = useState(null);
 
@@ -174,7 +181,7 @@ export default function UpdateShippingCostsGrid() {
 					display: "flex",
 					justifyContent: "center",
 					fontSize: "12px",
-					fontFamily: "Lexend Tera"
+					fontFamily: "Lexend Tera",
 				}}>
 					Current Shipping Costs
 				</div>
@@ -184,18 +191,16 @@ export default function UpdateShippingCostsGrid() {
 					display: "flex",
 					justifyContent: "flex-end",
 					fontSize: "11px",
-					fontFamily: "Lexend Tera"
+					fontFamily: "Lexend Tera",
 				}}>
-					{/* Filter Here */}
 
 					<Autocomplete
-						options={shippingDestinations}
-						// size="small"
-						getOptionLabel={(option) => option.destination_name || ''}
+						options={shippingDestinations.sort((a, b) => -b.destination_name.localeCompare(a.destination_name))}
+						getOptionLabel={(option) => option.destination_name}
 						getOptionSelected={(option, value) => option.destination_name === value.destination_name}
 						style={{ width: 175 }}
 						renderInput={(params) => <TextField {...params} label="Destination Filter" InputLabelProps={{ shrink: true }} />}
-						onChange={(event, value) => setStateFilter(value || '')}
+						onChange={(event, value) => handleStateFilter(value || null)}
 						value={stateFilter}
 					/>
 
@@ -206,6 +211,59 @@ export default function UpdateShippingCostsGrid() {
 
 
 
+	const CustomFooter = () => {
+
+		const handleChangePage = (event, newPage) => {
+			setPage(newPage);
+		}; // End handleChangePage
+
+		const handleChangeRowsPerPage = (event) => {
+			setRowsPerPage(parseInt(event.target.value, 10));
+			setPage(0);
+		}; // End handleChangeRowsPerPage
+
+
+		const PaginationComponent = () => (
+			<div style={{
+				flex: "1",
+				display: "flex",
+				justifyContent: "flex-end",
+				// fontSize: "11px",
+				// fontFamily: "Lexend Tera",
+			}}>
+				<TablePagination
+					component="div"
+					count={rows.length}
+					page={page}
+					onPageChange={handleChangePage}
+					rowsPerPage={rowsPerPage}
+					onRowsPerPageChange={handleChangeRowsPerPage}
+					rowsPerPageOptions={rowsPerPageOptions}
+
+				// pageSize={pageSize}
+				// onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+				// rowsPerPageOptions={[8, 16, 24, 48, 100]}
+				/>
+			</div>
+		); // End PaginationComponent
+
+
+
+
+		return (
+			<div style={{
+				flex: "1",
+				display: "flex",
+				justifyContent: "flex-start",
+			}}>
+				<PaginationComponent />
+			</div>
+		); // End return
+	}; // End CustomFooter
+
+
+	console.log(`Ryan Here 2: `, { selectedRow, shippingDestinations, stateFilter, rows, rowsToDisplay });
+
 	return (
 		<div
 			className={classes.shippingGrid}
@@ -213,14 +271,14 @@ export default function UpdateShippingCostsGrid() {
 			<DataGrid
 				className={classes.dataGridTables}
 				columns={columns}
-				rows={rows}
+				rows={rowsToDisplay}
 				getRowId={(row) => row.shipping_cost_id}
 				autoHeight
 				// ⬇ Pagination Setup: 
-				pagination
-				pageSize={pageSize}
-				onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-				rowsPerPageOptions={[8, 16, 24, 48, 100]}
+				// pagination
+				// pageSize={pageSize}
+				// onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+				// rowsPerPageOptions={[8, 16, 24, 48, 100]}
 				// ⬇ Selection Setup:
 				// onCellEditCommit={handleEditSubmit}
 				// hideFooter
@@ -234,8 +292,7 @@ export default function UpdateShippingCostsGrid() {
 				}}
 				components={{
 					Toolbar: CustomToolbar,
-					// Footer: CustomFooter,
-
+					Footer: CustomFooter,
 				}}
 			/>
 		</div>
