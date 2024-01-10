a76d134627f762a964cf673bcf15f9329adedac0
Ryan <ryan.maas@houserater.com>
2024-01-10 16:36:15 -0600

    Fixed classes bug in sys admin page.

--- src/components/AdminRegisterForm/AdminRegisterForm.jsx
+++ src/components/AdminRegisterForm/AdminRegisterForm.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import { useClasses } from '../MuiStyling/MuiStyling';
 
 function AdminRegisterForm() {
 
