7a8adfa08833c25c704031bf728efe1bfcf13f56
Ryan <ryan.maas@houserater.com>
2024-01-18 08:37:13 -0600

    Updated .env and PORT connection.

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use('/api/pricingLog', pricingLogRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.SERVER || 5000;
+const PORT = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(PORT, () => {
