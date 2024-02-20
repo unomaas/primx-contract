1a07621793ca1db8125842e76163f63905a89826
Ryan <ryan.maas@houserater.com>
2024-02-19 21:01:14 -0600

    Merged conflicts.

--- src/components/AdminRegisterForm/AdminRegisterForm.jsx
+++ src/components/AdminRegisterForm/AdminRegisterForm.jsx
@@ -7,7 +7,6 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { useClasses } from '../MuiStyling/MuiStyling';
 
-
 function AdminRegisterForm() {
 
   const errors = useSelector((store) => store.errors);
